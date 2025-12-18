e034457476e04edf237c85c64da18e7f1e775fc4
ArgentiLover <mofischl00@gmail.com>
2025-12-18 10:40:23 +0300

    fixing vercel deployment #1

--- src/app.js
+++ src/app.js
@@ -14,9 +14,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.resolve(__dirname, 'public')));
-console.log('Router test:', typeof wordsRoutes);
 app.use('/api/words', wordsRoutes);
 app.use('/api/train', trainRoutes);
+app.use('/api/start', (req, res, next) => {
+  req.url = '/start';
+  trainRoutes(req, res, next);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- src/routes/train.routes.js
+++ src/routes/train.routes.js
@@ -5,5 +5,6 @@ const controller = require('../controllers/words.controller');
 router.post('/start', controller.startTraining);
 router.get('/next', controller.getNext);
 router.post('/check', controller.checkAnswer);
+router.get('/random', controller.getRandom);
 
 module.exports = router;
